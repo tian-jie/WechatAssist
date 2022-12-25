72e2f2c946312c219ba12a9e9e6533782595332f
jie <jie.tian@outlook.com>
2022-12-25 15:03:43 +0800

    update script add log volumn

--- app/update-wechat4u.sh
+++ app/update-wechat4u.sh
@@ -2,4 +2,4 @@ git pull
 docker build -t wechat4u .
 docker kill wechat4u
 docker rm wechat4u
-docker run -itd -e DB_HOST=172.17.0.1 -e DB_NAME=wechat -e DB_PORT=1433 -e DB_USERNAME=sa -e DB_PASSWORD=Liaoningdalian1  -v /docker/data/wechat4u/media:/app/media --name wechat4u wechat4u
+docker run -itd -e DB_HOST=172.17.0.1 -e DB_NAME=wechat -e DB_PORT=1433 -e DB_USERNAME=sa -e DB_PASSWORD=Liaoningdalian1  -v /docker/data/wechat4u/media:/app/media  -v /docker/data/wechat4u/logs:/app/logs --name wechat4u wechat4u
